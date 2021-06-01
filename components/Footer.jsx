4ee91ca9a3ce9a84c9930d0a35fc2a8edfd4cdaa
jonathanriese <jonathanriese@gmail.com>
2021-06-01 23:55:29 +0200

    final updates

---
+++ components/Footer.jsx
@@ -0,0 +1,15 @@
+export const Footer = () => {
+    return (
+        <>
+            <footer>
+                <ul>
+                    <li>© 2021 — Evolve Academy</li>
+                    <li><a href="privacy.html">Privacy Policy</a></li>
+                    <li><a href="terms.html">Terms & Conditions</a></li>
+                    <li><a href="mailto:info@evolve-now.academy">Contact Us</a></li>
+                    <li><a href="register.html">Register</a></li>
+                </ul>
+            </footer>
+        </>
+    )
+  }
---
+++ components/Navbar.jsx
@@ -0,0 +1,9 @@
+export const Navbar = () => {
+    return (
+        <>
+            <header class="header nav-menu">
+                <a href="https://evolve-now.academy/">Evolve Academy</a><p>Member Space</p>
+            </header>
+        </>
+    )
+  }
